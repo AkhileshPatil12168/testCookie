04223b10d4f493212e574313b955a058baa718ed
Akhilesh Patil <akhileshpatil12168@gmail.com>
2023-07-29 19:56:39 +0530

    complete back

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+
+app.use(cors({
+  origin: 'https://testcookiefront.onrender.com', 
+  credentials: true, 
+}));
+
+app.use(cookieParser());
+
+
+app.get('/set-cookie', (req, res) => {
+  res.cookie('backend_cookie', 'cookie_value', { httpOnly: true, sameSite: 'none', secure: true }); 
+  res.send('Cookie set by backend');
+});
+
+app.listen(3000, () => {
+  console.log('Backend server running on https://testcookieback.onrender.com');
+});
